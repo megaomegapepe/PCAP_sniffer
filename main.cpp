80e8cd3e4ba96b484b93b3ab3d93fcd09af6c5cd
unknown <mr.safik322@gmail.com>
2021-06-01 15:43:57 +0700

    mod

--- main.cpp
+++ main.cpp
@@ -4,33 +4,30 @@
 /* Ethernet адреса состоят из 6 байт */
 #define ETHER_ADDR_LEN 6
 
-/* Заголовки Ethernet всегда состоят из 14 байтов */
-#define SIZE_ETHERNET 14
-
-/* Заголовок Ethernet */
+/* Ethernet header */
 struct sniff_ethernet
 {
-	u_char ether_dhost[ETHER_ADDR_LEN]; /* Адрес назначения */
-	u_char ether_shost[ETHER_ADDR_LEN]; /* Адрес источника */
-	u_short ether_type;					/* IP? ARP? RARP? и т.д. */
+	u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
+	u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
+	u_short ether_type;					/* IP? ARP? RARP? etc */
 };
 
 /* IP header */
 struct sniff_ip
 {
-	u_char ip_vhl;				   /* версия << 4 | длина заголовка >> 2 */
-	u_char ip_tos;				   /* тип службы */
-	u_short ip_len;				   /* общая длина */
-	u_short ip_id;				   /* идентефикатор */
-	u_short ip_off;				   /* поле фрагмента смещения */
-#define IP_RF 0x8000			   /* reserved флаг фрагмента */
-#define IP_DF 0x4000			   /* dont флаг фрагмента */
-#define IP_MF 0x2000			   /* more флаг фрагмента */
-#define IP_OFFMASK 0x1fff		   /* маска для битов фрагмента */
-	u_char ip_ttl;				   /* время жизни */
-	u_char ip_p;				   /* протокол */
-	u_short ip_sum;				   /* контрольная сумма */
-	struct in_addr ip_src, ip_dst; /* адрес источника и адрес назначения */
+	u_char ip_vhl;				   /* version << 4 | header length >> 2 */
+	u_char ip_tos;				   /* type of service */
+	u_short ip_len;				   /* total length */
+	u_short ip_id;				   /* identification */
+	u_short ip_off;				   /* fragment offset field */
+#define IP_RF 0x8000			   /* reserved fragment flag */
+#define IP_DF 0x4000			   /* don't fragment flag */
+#define IP_MF 0x2000			   /* more fragments flag */
+#define IP_OFFMASK 0x1fff		   /* mask for fragmenting bits */
+	u_char ip_ttl;				   /* time to live */
+	u_char ip_p;				   /* protocol */
+	u_short ip_sum;				   /* checksum */
+	struct in_addr ip_src, ip_dst; /* source and dest address */
 };
 #define IP_HL(ip) (((ip)->ip_vhl) & 0x0f)
 #define IP_V(ip) (((ip)->ip_vhl) >> 4)
@@ -40,11 +37,11 @@ typedef u_int tcp_seq;
 
 struct sniff_tcp
 {
-	u_short th_sport; /* порт источника */
-	u_short th_dport; /* порт назначения */
-	tcp_seq th_seq;	  /* номер последовательности */
-	tcp_seq th_ack;	  /* номер подтверждения */
-	u_char th_offx2;  /* смещение данных, rsvd */
+	u_short th_sport; /* source port */
+	u_short th_dport; /* destination port */
+	tcp_seq th_seq;	  /* sequence number */
+	tcp_seq th_ack;	  /* acknowledgement number */
+	u_char th_offx2;  /* data offset, rsvd */
 #define TH_OFF(th) (((th)->th_offx2 & 0xf0) >> 4)
 	u_char th_flags;
 #define TH_FIN 0x01
@@ -56,61 +53,50 @@ struct sniff_tcp
 #define TH_ECE 0x40
 #define TH_CWR 0x80
 #define TH_FLAGS (TH_FIN | TH_SYN | TH_RST | TH_ACK | TH_URG | TH_ECE | TH_CWR)
-	u_short th_win; /* окно */
-	u_short th_sum; /* контрольная сумма */
-	u_short th_urp; /* экстренный указатель */
+	u_short th_win; /* window */
+	u_short th_sum; /* checksum */
+	u_short th_urp; /* urgent pointer */
 };
+#define SIZE_ETHERNET 14
 
-void callback(u_char *useless, const struct pcap_pkthdr *pkthdr, const u_char *packet)
+const struct sniff_ethernet *ethernet; /* The ethernet header */
+const struct sniff_ip *ip;			   /* The IP header */
+const struct sniff_tcp *tcp;		   /* The TCP header */
+const char *payload;				   /* Packet payload */
+
+u_int size_ip;
+u_int size_tcp;
+
+void mycallback(u_char *useless, const struct pcap_pkthdr *pkthdr, const u_char *packet)
 {
-	static int count = 1;
-	const struct sniff_ethernet *ethernet; /* Заголовок Ethernet */
-	const struct sniff_ip *ip;			   /* Заголовок IP */
-	const struct sniff_tcp *tcp;		   /* Заголовок TCP */
-	u_char *payload = 0;				   /* Данные пакета */
+	const struct sniff_ethernet *ethernet; /* The ethernet header */
+	const struct sniff_ip *ip;			   /* The IP header */
+	const struct sniff_tcp *tcp;		   /* The TCP header */
+	const u_char *payload;				   /* Packet payload */
 
-	u_int size_ip;
-	u_int size_tcp;
+	int size_ethernet = sizeof(struct sniff_ethernet);
+	int size_ip = sizeof(struct sniff_ip);
+	int size_tcp = sizeof(struct sniff_tcp);
 
 	ethernet = (struct sniff_ethernet *)(packet);
-	ip = (struct sniff_ip *)(packet + SIZE_ETHERNET);
-	size_ip = IP_HL(ip) * 4;
-	if (size_ip < 20)
-	{
-		printf("   * Invalid IP header length: %u bytes\n", size_ip);
-		return;
-	}
-	tcp = (struct sniff_tcp *)(packet + SIZE_ETHERNET + size_ip);
-	size_tcp = TH_OFF(tcp) * 4;
-	if (size_tcp < 20)
-	{
-		//printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
-		return;
-	}
-	payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
+	ip = (struct sniff_ip *)(packet + size_ethernet);
+	tcp = (struct sniff_tcp *)(packet + size_ethernet + size_ip);
+	payload = (u_char *)(packet + size_ethernet + size_ip + size_tcp);
 
-	if (ethernet->ether_type != 8)
+	FILE *file = fopen("output", "ab");
+	if (file == NULL)
 	{
-		printf("HANDLED PACKET INFO:\nIP dir:%s\nIP source:%s\nIP type:%d\ncount:%d\n", ethernet->ether_dhost, ethernet->ether_shost, ethernet->ether_type, count);
+		printf("error opening file");
 	}
-	//printf("HANDLED PACKET INFO:\nIP dir:%s\nIP source:%s\nIP type:%d\ncount:%d\n", ethernet->ether_dhost, ethernet->ether_shost, ethernet->ether_type, count);
-	//printf("PACKET HANDLED:%s", payload);
 
-	//printf("PACKET CATHED:\ntime:%d\nCaplen:%d\nLen:%d\n", pkthdr->ts, (int)(pkthdr->caplen), (int)(pkthdr->len));
-	count++;
+	fwrite(&payload, sizeof(u_char), 1, file);
+	//fprintf(file, "Packet handled:%s; Packet type: %d\n", &payload, ethernet->ether_type);
+	printf("Packet handled:%s; Packet type: %d\n", (char *)payload, ethernet->ether_type);
+	fclose(file);
 }
 
 int main(int argc, char *argv[])
 {
-
-	const struct sniff_ethernet *ethernet; /* Заголовок Ethernet */
-	const struct sniff_ip *ip;			   /* Заголовок IP */
-	const struct sniff_tcp *tcp;		   /* Заголовок TCP */
-	u_char *payload = 0;				   /* Данные пакета */
-
-	u_int size_ip;
-	u_int size_tcp;
-
 	char *dev, errbuf[PCAP_ERRBUF_SIZE];
 
 	dev = pcap_lookupdev(errbuf);
@@ -133,7 +119,7 @@ int main(int argc, char *argv[])
 	struct pcap_pkthdr header;
 	const u_char *packet; /* Пакет */
 
-	pcap_loop(handle, -1, callback, NULL);
+	pcap_loop(handle, -1, mycallback, NULL);
 
 	pcap_close(handle);
 	return (0);
